77d57fec68d39adabb778e63150b4c20c0f0c666
GabriellaFGuerra <gabriella.guerra@etec.sp.gov.br>
2021-02-16 10:10:54 -0300

    Raw routing, no views nor controllers

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'home.index')->name('home');
+
+Route::get('/blueprints', function () {
+    return view('blueprints.index');
+});
+Route::get('/blueprints/{slug}', function () {
+    return view('blueprints.blueprint');
+});
+
+Route::get('/documents', function () {
+    return view('documents.index');
+});
+Route::get('/documents/{slug}', function () {
+    return view('documents.document');
+});
+
+Route::get('/projects', function () {
+    return view('projects.index');
+});
+Route::get('/projects/{slug}', function () {
+    return view('projects.project');
+});
+
+Route::get('/purchases', function () {
+   return view('purchases.index');
+});
+
+Route::get('/stock', function () {
+    return view('stock.index');
 });
